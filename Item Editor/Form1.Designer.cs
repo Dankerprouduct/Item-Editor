d0cdc8fa67f0eb8dcdf90e26c1407329e813ea31
David William Fitzgerald <Dankerprouduct2@gmail.com>
2015-11-26 01:01:41 -0500

    Item Editor
    this can only work on my computer unless you change the element xdoc
    source to whatever directory you want

---
+++ Item Editor/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Item_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.descTextbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.effectTextbos = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(15, 477);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(75, 23);
+            this.newButton.TabIndex = 28;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(105, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(181, 39);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Item Editor";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 477);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(156, 267);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 17);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Description";
+            // 
+            // descTextbox
+            // 
+            this.descTextbox.Location = new System.Drawing.Point(12, 287);
+            this.descTextbox.Multiline = true;
+            this.descTextbox.Name = "descTextbox";
+            this.descTextbox.Size = new System.Drawing.Size(371, 119);
+            this.descTextbox.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(178, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 17);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Effect Amount";
+            // 
+            // effectTextbos
+            // 
+            this.effectTextbos.Location = new System.Drawing.Point(12, 206);
+            this.effectTextbos.Name = "effectTextbos";
+            this.effectTextbos.Size = new System.Drawing.Size(160, 22);
+            this.effectTextbos.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 17);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Effect";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 176);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(207, 24);
+            this.comboBox2.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Type";
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Location = new System.Drawing.Point(12, 146);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(207, 24);
+            this.comboBoxType.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Name";
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Location = new System.Drawing.Point(12, 118);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(210, 22);
+            this.nameTextbox.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 513);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.descTextbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.effectTextbos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxType);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox descTextbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox effectTextbos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextbox;
+    }
+}
+
---
+++ Item Editor/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Item_Editor
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument xDoc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.Text = "Item Editor";
+            comboBoxType.Items.Add("NULL");
+            comboBoxType.Items.Add("Weapon");
+            comboBoxType.Items.Add("Consumable");
+            comboBox2.Items.Add("NULL"); 
+            comboBox2.Items.Add("Fire");
+            comboBox2.Items.Add("Health");
+            comboBox2.Items.Add("Damage");
+
+            comboBoxType.SelectedItem = "NULL";
+            comboBox2.SelectedItem = "NULL";
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+
+            comboBoxType.SelectedItem = "NULL";
+            comboBox2.SelectedItem = "NULL";
+            nameTextbox.Text = "NULL";
+            descTextbox.Text = "NULL";
+            effectTextbos.Text = "NULL";
+        }
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            xDoc = new XmlDocument();
+            xDoc.Load(@"C:\Users\David\Documents\XML files\Item.xml");
+
+            XmlNode node = xDoc.CreateNode(XmlNodeType.Element, "Item", null);
+            XmlNode nameNode = xDoc.CreateElement("Name");
+            nameNode.InnerText = nameTextbox.Text;
+            XmlNode typeNode = xDoc.CreateElement("Type");
+            typeNode.InnerText = comboBoxType.SelectedItem.ToString();
+            XmlNode effectNode = xDoc.CreateElement("Effect");
+            effectNode.InnerText = comboBox2.SelectedItem.ToString();
+            XmlNode powerNode = xDoc.CreateElement("Power");
+            powerNode.InnerText = effectTextbos.Text;
+            XmlNode descNode = xDoc.CreateElement("Desc");
+            descNode.InnerText = descTextbox.Text;
+
+            node.AppendChild(nameNode);
+            node.AppendChild(typeNode);
+            node.AppendChild(effectNode);
+            node.AppendChild(powerNode);
+            node.AppendChild(descNode);
+
+            xDoc.DocumentElement.AppendChild(node);
+            xDoc.Save(@"C:\Users\David\Documents\XML files\Item.xml");
+
+            comboBoxType.SelectedItem = "NULL";
+            comboBox2.SelectedItem = "NULL";
+            nameTextbox.Text = "NULL";
+            descTextbox.Text = "NULL";
+            effectTextbos.Text = "NULL";
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            comboBoxType.SelectedItem = "NULL";
+            comboBox2.SelectedItem = "NULL";
+            nameTextbox.Text = "NULL";
+            descTextbox.Text = "NULL";
+            effectTextbos.Text = "NULL";
+        }
+    }
+}
